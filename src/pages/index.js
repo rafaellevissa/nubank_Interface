8affafabceb4f67a551442aa83dbf7445811c1de
rafaellevissa <rafaellevissa@gmail.com>
2020-07-17 16:55:57 -0300

    :bug: animated event

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ export default function Home(){
         [
             {
                 nativeEvent:{
-                    translationY = translateY,
+                    translationY: translateY,
                 },
             },
         ], 
